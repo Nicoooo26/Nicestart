c2c211b19643298e6d92382b06219011432da12b
Nicoooo26 <nicogpfutbol@gmail.com>
2025-11-11 13:41:33 +0100

    change Splash Activity in branch Stack

--- app/src/main/java/com/example/nicestart/Splash.java
+++ app/src/main/java/com/example/nicestart/Splash.java
@@ -28,8 +28,8 @@ public class Splash extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(Splash
                         .this, Login.class);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
         }, 5000);
