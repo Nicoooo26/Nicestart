8f8f2629c3f107593aa0c2f20b6d03655d316d70
Nicoooo26 <nicogpfutbol@gmail.com>
2025-11-20 10:16:31 +0100

    add app bar,context menu and swipe in branch Stack

--- app/src/main/java/com/example/nicestart/Main.java
+++ app/src/main/java/com/example/nicestart/Main.java
@@ -1,19 +1,123 @@
 package com.example.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Main extends AppCompatActivity {
+
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView mycontext = findViewById(R.id.mycontext);
+        registerForContextMenu(mycontext);
+
+        swipeLayout = findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
     }
+
+// IMPLEMENTING APP BAR MENU
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.settings) {
+//            showAlertDialogButtonClicked(Main.this);
+
+            Toast toast = Toast.makeText(this, "Infecting", Toast.LENGTH_LONG);
+            toast.show();
+
+        }
+        if (id == R.id.copy) {
+            Toast toast = Toast.makeText(this, "Fixing", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        if (id == R.id.item3) {
+//            Intent intent = new Intent(Main.this, MainBab.class);
+//            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    // IMPLEMENTING CONTEXT MENU
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+//        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)
+//                item.getMenuInfo();
+        if (item.getItemId() == R.id.item1) {
+            Toast toast = Toast.makeText(this, "Item copied",
+                    Toast.LENGTH_LONG);
+            toast.show();
+        } else if (item.getItemId() == R.id.item2) {
+            Toast toast2 = Toast.makeText(this, "Downloading item...",
+                    Toast.LENGTH_LONG);
+            toast2.show();
+        }
+        return false;
+    }
+
+    protected SwipeRefreshLayout.OnRefreshListener
+        mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener(){
+        @Override
+        public void onRefresh() {
+//            Toast toast0 = Toast.makeText(Main.this, "Hi there! I don't exist :)", Toast.LENGTH_LONG);
+//            toast0.show();
+
+            final ConstraintLayout mLayout = findViewById(R.id.myMainConstraint);
+//
+            Snackbar snackbar = Snackbar
+                    .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_SHORT)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                            snackbar1.show();
+                        }
+                    });
+
+            snackbar.show();
+            swipeLayout.setRefreshing(false);
+        }
+    };
+
 }
+
+
